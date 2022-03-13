012754520f61a098f711a7ac389bb521859dbcd3
薛勻婷 <cary910727@gmail.com>
2022-03-13 16:33:51 +0800

    3.11 pratice

--- src/main/java/com/xue/Student.java
+++ src/main/java/com/xue/Student.java
@@ -4,4 +4,24 @@ public class Student {
     String name;
     int english;
     int math;
+
+    public Student(String name) //constructing
+    {
+        this.name = name;
+    }
+    public Student(String name,int english,int math)
+    {
+        this(name);
+        this.english = english;
+        this.math = math;
+    }
+    public Student()
+    {
+
+    }
+    public void print()
+    {
+        System.out.println(name + "\t" + english + "\t" + math +
+                "\t");
+    }
 }
