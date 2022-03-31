5ccc587e2b77920ee41e00419b0dc57e65060efd
薛勻婷 <cary910727@gmail.com>
2022-03-31 21:38:50 +0800

    3.31

--- src/main/java/com/xue/ticket/Sales.java
+++ src/main/java/com/xue/ticket/Sales.java
@@ -8,12 +8,18 @@ public class Sales {
         new NormalTicket(Station.TAIPEI,Station.KAOHSIUNG, 2);
     Scanner scanner = new Scanner(System.in);
         System.out.println(
-                "Please enter the StartStation (TAIPEI : 100 , TAICHUNG : 200 , KAOHSIUNG : 300)");
+                "Please enter the startstation (TAIPEI : 100 , TAICHUNG : 200 , KAOHSIUNG : 300)");
        int choice = Integer.parseInt(scanner.next());
-       Station stratstation = null;
+       Station startstation = null;
        switch (choice){
            case 100:
-
+               startstation = Station.TAIPEI;
+               break;
+           case 200:
+               startstation = Station.TAICHUNG;
+               break;
+           case 300:
+               startstation = Station.KAOHSIUNG;
                break;
        }
     }
---
+++ src/main/java/com/xue/ticket/Tester.java
@@ -0,0 +1,13 @@
+package com.xue.ticket;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Please enter the startstation" +
+                "(Taipei：100, Taichung：200, Kaosiung：300)");
+        Scanner scanner = new Scanner(System.in);
+        int choice = Integer.parseInt(scanner.next());
+
+    }
+}
