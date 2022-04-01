75a5c8ab3f6c13427daaaa104d0b834b3d0eaf90
xue <cary910727@gmail.com>
2022-04-01 12:55:26 +0800

    4/1

--- src/main/java/com/xue/score/Scoring.java
+++ src/main/java/com/xue/score/Scoring.java
@@ -1,12 +1,19 @@
 package com.xue.score;
 
+import com.xue.sales.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoring {
     public static void main(String[] args) {
-        Student Ruby = new Student("Ruby",80,65);
-        Ruby.print();
-        Student Elsa = new Student("Elsa",76,34);
-        Elsa.print();
-        GraduteStudent Tony = new GraduteStudent("Tony",60,78,88);
-        Tony.print();
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ruby",80,65));
+        students.add(new Student("Elsa",76,34));
+        students.add(new GraduteStudent("Tony",60,78,88));
+
+        for (Student s:students) {
+            s.print();
+        }
     }
 }
