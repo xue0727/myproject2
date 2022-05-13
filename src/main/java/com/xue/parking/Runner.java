e5b3b5dfaacfae3c013e2d37f716561d43cb18fd
薛勻婷 <cary910727@gmail.com>
2022-05-13 14:59:53 +0800

    5/13

---
+++ src/main/java/com/xue/parking/Runner.java
@@ -0,0 +1,46 @@
+package com.xue.parking;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Runner {
+    public static void main(String[] args) {
+        String id = "ABC-1234";
+        String enterTime = "11:55";
+        String exitTime = "14:03";
+        SimpleDateFormat sdf =
+                new SimpleDateFormat("HH:mm");
+        //Exception 例外
+        //Exception handling 例外處理
+        Date d = null;
+        try {
+            d = sdf.parse(enterTime);
+        }catch (Exception e){
+            System.out.println("Wrong format");
+        }
+        System.out.println(d);
+        System.out.println(d.getTime());
+
+        //long a = 3*60*60*1000;
+        try{
+            Date d2 = sdf.parse(exitTime);
+            System.out.println(d2);
+            System.out.println(d2.getTime());
+            System.out.println(d.before(d2));
+            long ms = d2.getTime() - d.getTime();
+            int minutes = (int)(ms/(1000*60));
+            System.out.println(minutes);
+            //1小時30元
+            int hours = (minutes+30)/60;
+            System.out.println(30*hours);
+            //不滿15分鐘不算錢
+            //15-29分鐘算為半小時，收15元
+            //30-44分鐘算為半小時，收15元
+            //繳費完成後15分鐘內需離場
+        }catch (Exception e){
+            System.out.println("Wrong format");
+        }
+        //d2.setTime(d.getTime()+a);
+
+    }
+}
