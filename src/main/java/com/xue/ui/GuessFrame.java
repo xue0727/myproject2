ceda8bb87ffcb0721ae2632e394e0dd597cbb778
薛勻婷 <cary910727@gmail.com>
2022-05-27 13:01:36 +0800

    5/27 視窗的猜數字練習

--- src/main/java/com/xue/ui/GuessFrame.java
+++ src/main/java/com/xue/ui/GuessFrame.java
@@ -6,29 +6,46 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessFrame extends JFrame {
     //Fields屬性
-    JButton button = new JButton("TRY");
-    JLabel label = new JLabel("Good");
-    JLabel label2 = new JLabel("Don't press the button！！");
+    JButton button = new JButton("GUESS");
+    JLabel label = new JLabel("START");
+    //JLabel label2 = new JLabel("Don't press the button！！");
+    JTextField number = new JTextField(10);
+
     //Constructors建構子
     public GuessFrame(){
         super();
-        setTitle("Press the button");
+        setTitle("Guess the number 1-10");
         setSize(500,300);
         setLocation(200,100);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        Random random = new Random();
+        int secret = random.nextInt(10)+1;
+        System.out.println(secret);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 //System.out.println("Fool!");
-                label.setText("Fool！");
+                //label.setText("Fool！");
+                int num = Integer.parseInt(number.getText());
+                //System.out.println(num);
+                if (num > secret){
+                    label.setText("Smaller");
+                }else if(num < secret){
+                    label.setText("Bigger");
+                }else{
+                    label.setText("BINGO！The secret number is " + secret);
+                }
             }
         });
         setLayout(new FlowLayout());
-        add(label2);
+        //add(label2);
+        add(number);
         add(button);
         add(label);
         setVisible(true);
